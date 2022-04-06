96391dc629b5d742765eb614669d63f0da9ec0eb
Douglas B <guod1977@gmail.com>
2022-04-06 12:25:08 -0700

    style done- needs cleanup and refactoring

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import {
   Grid,
   Box,
@@ -7,7 +7,9 @@ import {
   Button,
   FormControl,
   TextField,
-} from '@material-ui/core';
+} from "@material-ui/core";
+import bgImage from "./images/bg-img.png";
+import bubble from "./images/bubble.svg";
 
 const Login = ({ user, login }) => {
   const history = useHistory();
@@ -23,47 +25,157 @@ const Login = ({ user, login }) => {
   };
 
   useEffect(() => {
-    if (user && user.id) history.push('/home');
+    if (user && user.id) history.push("/home");
   }, [user, history]);
 
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Link href="/register" to="/register">
-            <Button>Register</Button>
-          </Link>
-        </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
+    <Box sx={{ display: "flex" }}>
+      <Box
+        className="bgImage"
+        alt="Chat picture"
+        sx={{
+          height: "100vh",
+          width: "75%",
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          position: "relative",
+        }}
+      >
+        <div
+          className="overlay"
+          style={{
+            backgroundColor: "rgba(58,141,255,.8)",
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        ></div>
+        <div
+          className="leftContainer"
+          style={{
+            position: "absolute",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            width: "75%",
+            left: "50%",
+            top: "40%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <div
+            className="chatBubble"
+            style={{
+              backgroundImage: `url(${bubble})`,
+              backgroundSize: "cover",
+              height: "70px",
+              width: "70px",
+              marginBottom: "20px",
+              margin: "0 auto",
+            }}
+          ></div>
+          <div
+            className="tagText"
+            style={{
+              color: "white",
+              fontSize: "2em",
+              marginTop: "40px",
+            }}
+          >
+            Converse with anyone with any language
+          </div>
+        </div>
+      </Box>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Box
+          sx={{
+            width: "65%",
+            minHeight: "80vh",
+            flexDirection: "column",
+            p: "20px",
+          }}
+        >
+          <Grid
+            container
+            item
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "right",
+              alignItems: "center",
+              marginTop: "30px",
+            }}
+          >
+            <Typography color="secondary">Don't have an account?</Typography>
+            <Link
+              href="/register"
+              to="/register"
+              style={{ textDecoration: "none", marginLeft: "20px" }}
+            >
+              <Button
+                color="primary"
+                style={{ boxShadow: "0px 4px 4px 0px #B0B0B0", width: "150px" }}
+              >
+                Create account
+              </Button>
+            </Link>
+          </Grid>
+          <form onSubmit={handleLogin}>
+            <Grid
+              style={{
+                // height: "400px",
+                marginTop: "100px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h4" style={{ fontWeight: "bolder" }}>
+                Welcome back!
+              </Typography>
+              <Grid>
+                <FormControl fullWidth margin="normal" required>
+                  <TextField
+                    aria-label="username"
+                    label="Username"
+                    name="username"
+                    type="text"
+                  />
+                </FormControl>
+              </Grid>
+              <FormControl fullWidth margin="normal" required>
                 <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
+                  label="password"
+                  aria-label="password"
+                  type="password"
+                  name="password"
                 />
               </FormControl>
+              <Grid container justifyContent="center">
+                <Button
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  style={{ width: "150px", marginTop: "50px"}}
+                >
+                  Login
+                </Button>
+              </Grid>
             </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Grid>
+          </form>
+        </Box>
+      </Grid>
+    </Box>
   );
 };
 
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import {
   Grid,
   Box,
@@ -8,7 +8,9 @@ import {
   FormControl,
   TextField,
   FormHelperText,
-} from '@material-ui/core';
+} from "@material-ui/core";
+import bgImage from "./images/bg-img.png";
+import bubble from "./images/bubble.svg";
 
 const Signup = ({ user, register }) => {
   const history = useHistory();
@@ -25,86 +27,204 @@ const Signup = ({ user, register }) => {
     const confirmPassword = formElements.confirmPassword.value;
 
     if (password !== confirmPassword) {
-      setFormErrorMessage({ confirmPassword: 'Passwords must match' });
+      setFormErrorMessage({ confirmPassword: "Passwords must match" });
       return;
     }
     await register({ username, email, password });
   };
 
   useEffect(() => {
-    if (user && user.id) history.push('/home');
+    if (user && user.id) history.push("/home");
   }, [user, history]);
 
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Link href="/login" to="/login">
-            <Button>Login</Button>
-          </Link>
-        </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
-        </form>
+    <Box sx={{ display: "flex" }}>
+      <Box
+        className="bgImage"
+        alt="Chat picture"
+        sx={{
+          height: "100vh",
+          width: "75%",
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          position: "relative"
+        }}
+      >
+        <div
+          className="overlay"
+          style={{
+            backgroundColor: "rgba(58,141,255,.8)",
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        ></div>
+        <div
+          className="leftContainer"
+          style={{
+            position: "absolute",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            width: "75%",
+            left: "50%",
+            top: "40%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <div
+            className="chatBubble"
+            style={{
+              backgroundImage: `url(${bubble})`,
+              backgroundSize: "cover",
+              height: "70px",
+              width: "70px",
+              marginBottom: "20px",
+              margin: "0 auto",
+            }}
+          ></div>
+          <div
+            className="tagText"
+            style={{
+              color: "white",
+              fontSize: "2em",
+              marginTop: "40px",
+            }}
+          >
+            Converse with anyone with any language
+          </div>
+        </div>
       </Box>
-    </Grid>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Box
+          sx={{
+            width: "65%",
+            minHeight: "80vh",
+            flexDirection: "column",
+            p: "20px",
+          }}
+        >
+          <Grid
+            container
+            item
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "right",
+              alignItems: "center",
+              marginTop: "30px",
+            }}
+          >
+            <Typography color="secondary">Already have an account?</Typography>
+            <Link
+              href="/login"
+              to="/login"
+              style={{ textDecoration: "none", marginLeft: "20px" }}
+            >
+              <Button
+                color="primary"
+                style={{ boxShadow: "0px 4px 4px 0px #B0B0B0", width: "100px" }}
+              >
+                Login
+              </Button>
+            </Link>
+          </Grid>
+          <form onSubmit={handleRegister}>
+            <Grid
+              style={{
+                height: "400px",
+                marginTop: "40px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h4" style={{ fontWeight: "bolder" }}>
+                Create an account.
+              </Typography>
+              <Grid>
+                <FormControl fullWidth>
+                  <TextField
+                    aria-label="username"
+                    label="Username"
+                    name="username"
+                    type="text"
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid>
+                <FormControl fullWidth>
+                  <TextField
+                    label="E-mail address"
+                    aria-label="e-mail address"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid>
+                <FormControl
+                  fullWidth
+                  error={!!formErrorMessage.confirmPassword}
+                >
+                  <TextField
+                    aria-label="password"
+                    label="Password"
+                    type="password"
+                    inputProps={{ minLength: 6 }}
+                    name="password"
+                    required
+                  />
+                  <FormHelperText>
+                    {formErrorMessage.confirmPassword}
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Grid>
+                <FormControl
+                  fullWidth
+                  error={!!formErrorMessage.confirmPassword}
+                >
+                  <TextField
+                    label="Confirm Password"
+                    aria-label="confirm password"
+                    type="password"
+                    inputProps={{ minLength: 6 }}
+                    name="confirmPassword"
+                    required
+                  />
+                  <FormHelperText>
+                    {formErrorMessage.confirmPassword}
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Grid container justifyContent="center">
+                <Button
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  style={{ width: "150px" }}
+                >
+                  Create
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Grid>
+    </Box>
   );
 };
 
